924251ea82e2fcfcfbaa4e676d45abda1fc37bf4
suharsonosyahputra <suharsono.syahputra@gmail.com>
2020-05-28 16:31:24 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     <title>Coba Git remote</title>
 </head>
 <body>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, sunt amet. Aliquam, praesentium illum nesciunt ea nobis atque alias iure dolorum illo ratione explicabo deserunt tempora hic optio voluptatum perferendis?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, sunt amet. Aliquam, praesentium illum nesciunt ea nobis atque alias iure dolorum illo ratione explicabo deserunt tempora hic optio voluptatum perferendis?
+    </p>
+    <li>sodium_crypto_kdf_derive_from_keysdnsk'd</li>
 </body>
 </html>
